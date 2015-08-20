6da56b84aff9e988da3f791f2eeb9eaa786d200e
camilo9020 <camilo9020@gmail.com>
2015-08-20 10:51:29 -0500

    Initial commit

---
+++ db/migrate/20150820154034_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.time :duration
+      t.date :year
+      t.string :rating
+      t.text :description
+      t.url :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
